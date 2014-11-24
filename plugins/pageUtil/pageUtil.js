51f9b5cbbc69f618e1905c36432b82aeb46eefed
xiaoyuzhzh <xiaoyuzhzh@163.com>
2014-11-24 14:21:51 +0800

    测试提交改动

---
+++ plugins/pageUtil/pageUtil.js
@@ -0,0 +1,9 @@
+/* ===========================================================
+ * pageUtil.js v1.0
+ * Core code of pageUtil plugin
+ * https://github.com/xiaoyuzhzh/iceberg
+ * ===========================================================
+ * Author : xiaoyuzhzh
+ *          Twitter : @xiaoyuzhzh
+ *          Website :
+ */
